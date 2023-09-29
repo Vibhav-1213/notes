1bb8978b6d914b50eb74f5c9f4274ef0609868be
Vibhav-1213 <119440423+Vibhav-1213@users.noreply.github.com>
2023-09-29 10:48:37 +0530

    Update file.dart

--- file.dart
+++ file.dart
@@ -15,6 +15,22 @@ class FilePage extends StatefulWidget {
 class _FilePageState extends State<FilePage> {
   TextEditingController title1 = new TextEditingController();
   TextEditingController note1 = new TextEditingController();
+  TextEditingController date1 = new TextEditingController();
+  bool? isChecked = false;
+  DateTime _dateTime = DateTime.now();
+  void _showDatePicker(){
+    showDatePicker(
+      context: context, 
+      initialDate: DateTime.now(), 
+      firstDate: DateTime(2000), 
+      lastDate: DateTime(2025)).then(
+        (value) {
+          setState(() {
+            _dateTime = value!;
+          });
+        }
+        );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,7 +50,7 @@ class _FilePageState extends State<FilePage> {
               Padding(
                 padding: EdgeInsets.only(right:17,top: 0),
                 child: IconButton(onPressed: (){
-                  Map <String,dynamic> data = {"Title":title1.text,"Content":note1.text};
+                  Map <String,dynamic> data = {"Title":title1.text,"Content":note1.text,"Date":_dateTime.toString()};
                   FirebaseFirestore.instance.collection("notes").add(data);
                   Navigator.pop(context);
                 }, icon: ImageIcon(AssetImage('images/tick.png'),size: 25,)))
@@ -47,6 +63,7 @@ class _FilePageState extends State<FilePage> {
                   padding: EdgeInsets.only(top:0,left: 15,right: 20,bottom: 10),
                   child: TextField(
                     controller: title1,
+                    maxLines: null,
                     decoration: InputDecoration(
                       hintText: "Title",
                       hintStyle: TextStyle(color: Colors.white,fontSize: 34)
@@ -63,6 +80,7 @@ class _FilePageState extends State<FilePage> {
                 Padding(
                   padding: EdgeInsets.only(top:0,left: 22.5,right: 20,bottom: 10),
                   child: TextField(
+                    maxLines: null,
                     controller: note1,
                     decoration: InputDecoration(
                       hintText: "Note",
@@ -74,6 +92,21 @@ class _FilePageState extends State<FilePage> {
               ],
             ),
           ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.only(left: 20,right: 20,top: 100),
+              child: ElevatedButton(
+                onPressed: _showDatePicker, 
+                child: Text("Choose Date",style: TextStyle(color: Colors.white,fontSize: 20),),),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.only(left: 20,right: 20,top: 250),
+              child: Align(
+                alignment: Alignment.center,
+                child: Text("Time : " + _dateTime.toString(),style: TextStyle(color: Colors.grey,fontSize: 12),))),
+          )
         ],
       ),
     );
